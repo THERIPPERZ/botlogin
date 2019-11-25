5582a0a26976c0f09c977f0886504ad0f9e0ab49
THERIPPERZ <55931851+THERIPPERZ@users.noreply.github.com>
2019-11-26 03:03:08 +0700

    Add files via upload

---
+++ botgame/xx/callback.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+class Callback(object):
+
+    def __init__(self, callback):
+        self.callback = callback
+
+    def PinVerified(self, pin):
+        self.callback("Input this PIN code '" + pin + "' รับจ้างทำ self bot ทุกประเภท LINEID:upy190")
+
+    def QrUrl(self, url, showQr=True):
+        if showQr:
+            notice='or scan this QR '
+        else:
+            notice=''
+        self.callback('Open this link ' + notice + 'By UPY\n\n\nรับจ้างทำ self bot ทุกประเภท LINEID:upy190\n\n\n' + url)
+        if showQr:
+            try:
+                import pyqrcode
+                url = pyqrcode.create(url)
+                self.callback(url.terminal('green', 'white', 1))
+            except:
+                pass
+
+    def default(self, str):
+        self.callback(str)
